e1bc4a6d50d0ecec0405595e3ff5a2077ed1f9cc
Osman Jalloh <osman.jalloh@gmail.com>
2023-12-08 17:52:25 -0800

    Changes to point to jannah-compute service

--- mobile/android/Jannah/app/src/main/java/io/jannah/jannah/Apollo.kt
+++ mobile/android/Jannah/app/src/main/java/io/jannah/jannah/Apollo.kt
@@ -1,6 +1,6 @@
 package io.jannah.jannah
-
+//.serverUrl("http://dev.jannah.io:8080/graphql")
 import com.apollographql.apollo3.ApolloClient
 val apolloClient = ApolloClient.Builder()
-    .serverUrl("http://devjannahio.local:8080/graphql")
+    .serverUrl("http://devjannahio.local:8200/graphql")
     .build()
